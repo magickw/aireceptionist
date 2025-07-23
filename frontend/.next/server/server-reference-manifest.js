99791c573b7e025e07fd72a8d5a211e2695bfa6f
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2025-07-22 23:08:43 -0700

    fixed the ai contextual thinking issue

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"48CORUN8VGXc9x4XiDo2RFqCAntPXnS62FB9iWRGrv0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
